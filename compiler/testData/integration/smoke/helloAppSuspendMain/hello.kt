d64b25c942d1e56cdd4558cb7fe953163a748605
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-09-06 11:21:10 +0300

    Support suspend fun main in JVM
     #KT-17679 Fixed

---
+++ compiler/testData/integration/smoke/helloAppSuspendMain/hello.kt
@@ -0,0 +1,36 @@
+package Hello
+
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
+import kotlin.coroutines.resume
+import kotlin.reflect.jvm.javaMethod
+
+@kotlin.jvm.Volatile
+private var result = ""
+@kotlin.jvm.Volatile
+private var callback: Function0<Unit>? = null
+
+suspend fun appendAndSuspend(s: String) {
+    result += s
+
+    suspendCoroutine<Unit> { continuation ->
+        callback = {
+            continuation.resume(Unit)
+        }
+    }
+}
+
+suspend fun main(args: Array<String>) {
+    thread(isDaemon = true) {
+        while (true) {
+            val c = callback
+            c?.invoke()
+            Thread.sleep(500)
+        }
+    }
+
+    appendAndSuspend(args[0])
+    appendAndSuspend(args[1])
+    println(result)
+    callback = null
+}
